eaf89b1a1b90d78ba5064d7681c432a8d6300ca3
向振华 <1092223021@qq.com>
2021-06-24 11:17:25 +0800

    增加vuex-persistedstate

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,14 @@
+/*
+ * @Descripttion:
+ * @LastEditors: xzh
+ * @LastEditTime: 2021-06-24 11:17:02
+ */
 import { createStore } from "vuex";
 import user from "./modules/user";
 import app from "./modules/app";
 import permission from "./modules/permission";
 import getters from "./getters";
+import createPersistedState from "vuex-persistedstate";
 
 export const store = createStore({
   modules: {
@@ -11,6 +17,11 @@ export const store = createStore({
     permission,
   },
   getters,
+  plugins: [
+    createPersistedState({
+      storage: window.sessionStorage,
+    }),
+  ],
 });
 
 export default store;
